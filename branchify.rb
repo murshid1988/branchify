da5ab372bea7a682ee136979632b51df105503b7
Murshid Ahmed <murshid1988@gmail.com>
2026-07-09 13:34:00 +0530

    Point Homebrew formula and PKGBUILD at v1.1.1 with computed sha256

--- branchify.rb
+++ branchify.rb
@@ -1,8 +1,8 @@
 class Branchify < Formula
   desc "Convert an arbitrary string into a valid git branch name"
   homepage "https://github.com/murshid1988/branchify"
-  url "https://github.com/murshid1988/branchify/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "cb636d91e53afa39a06ccc56b9ccbb1aa8202ed39b7934c545712e30cbf4e11f"
+  url "https://github.com/murshid1988/branchify/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "cef1017bbeb677d987efc4b798a7e3dd1eea90dffa9f8bdc4363b3becf23a93c"
   license "MIT"
 
   def install
